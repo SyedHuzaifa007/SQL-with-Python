c4fe2592331236a17f9c83e4c2ed033919f08954
SyedHuzaifa007 <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-01-30 23:52:35 -0800

    Update Testing SQL.py

--- Testing SQL.py
+++ Testing SQL.py
@@ -1,3 +1,4 @@
-import datetime
-date = datetime.date.today()
-print(date)
+age1 = int(input("What was your age when you joined this company: "))
+age2 = int(input("What is your current age: "))
+result = age2 - age1 
+print(f"You're Service of {result} year is completed successfully.")
